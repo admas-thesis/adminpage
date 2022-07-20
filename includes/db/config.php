593a1029beaaacb3f781645e0026fab30bcc1c99
Mint Developer <93377321+akram-seid@users.noreply.github.com>
2022-07-20 10:05:20 +0300

    Fixed attendance page course filtering

--- includes/db/config.php
+++ includes/db/config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'admas');
+define('DB_NAME', 'mydb');
  
 /* Attempt to connect to MySQL database */
 $conection_db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
